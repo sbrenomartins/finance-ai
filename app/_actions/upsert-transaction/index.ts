74518f15709ff67424c21939f30c3d73e028435e
Breno M. Silva <sbrenomartins@gmail.com>
2024-11-11 21:50:52 -0300

    feat: ensure transaction can be updated

--- app/_actions/upsert-transaction/index.ts
+++ app/_actions/upsert-transaction/index.ts
@@ -11,6 +11,7 @@ import { addTransactionSchema } from "./schema";
 import { revalidatePath } from "next/cache";
 
 interface AddTransactionParams {
+  id?: string;
   name: string;
   amount: number;
   type: TransactionType;
@@ -19,15 +20,19 @@ interface AddTransactionParams {
   date: Date;
 }
 
-export const addTransaction = async (params: AddTransactionParams) => {
+export const upsertTransaction = async (params: AddTransactionParams) => {
   addTransactionSchema.parse(params);
 
   const { userId } = await auth();
 
   if (!userId) throw new Error("Unauthorized.");
 
-  await db.transaction.create({
-    data: { ...params, userId },
+  await db.transaction.upsert({
+    where: {
+      id: params.id,
+    },
+    update: { ...params, userId },
+    create: { ...params, userId },
   });
 
   revalidatePath("/transactions");

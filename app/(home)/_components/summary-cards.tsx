962dfb7198ced6341bd4bc5031e35de090b7f1ce
Breno M. Silva <breno.martins@supermix.com.br>
2024-11-13 17:04:16 -0300

    feat: add summary cards

--- app/(home)/_components/summary-cards.tsx
+++ app/(home)/_components/summary-cards.tsx
@@ -7,11 +7,22 @@ import {
 import SummaryCard from "./summary-card";
 import { db } from "@/app/_lib/prisma";
 
-const SummaryCards = async () => {
+interface SummaryCardsProps {
+  month: string;
+}
+
+const SummaryCards = async ({ month }: SummaryCardsProps) => {
+  const where = {
+    date: {
+      gte: new Date(`2024-${month}-01`),
+      lt: new Date(`2024-${month}-31`),
+    },
+  };
+
   const depositsTotal = Number(
     (
       await db.transaction.aggregate({
-        where: { type: "DEPOSIT" },
+        where: { ...where, type: "DEPOSIT" },
         _sum: { amount: true },
       })
     )._sum.amount,
@@ -20,7 +31,7 @@ const SummaryCards = async () => {
   const investmentsTotal = Number(
     (
       await db.transaction.aggregate({
-        where: { type: "INVESTMENT" },
+        where: { ...where, type: "INVESTMENT" },
         _sum: { amount: true },
       })
     )._sum.amount,
@@ -29,7 +40,7 @@ const SummaryCards = async () => {
   const expensesTotal = Number(
     (
       await db.transaction.aggregate({
-        where: { type: "EXPENSE" },
+        where: { ...where, type: "EXPENSE" },
         _sum: { amount: true },
       })
     )._sum.amount,
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,18 +2,38 @@ import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import Navbar from "@/app/_components/navbar";
 import SummaryCards from "./_components/summary-cards";
+import TimeSelect from "./_components/time-select";
+import { isMatch } from "date-fns";
 
-const Home = async () => {
+interface HomeProps {
+  searchParams: {
+    month: string;
+  };
+}
+
+const Home = async ({ searchParams: { month } }: HomeProps) => {
   const { userId } = await auth();
 
   if (!userId) {
     return redirect("/auth/login");
   }
 
+  const monthIsInvalid = !month || !isMatch(month, "MM");
+
+  if (monthIsInvalid) {
+    return redirect("?month=01");
+  }
+
   return (
     <>
       <Navbar />
-      <SummaryCards />
+      <div className="space-y-6 p-6">
+        <div className="flex justify-between">
+          <h1 className="text-2xl font-bold">Dashboard</h1>
+          <TimeSelect />
+        </div>
+        <SummaryCards month={month} />
+      </div>
     </>
   );
 };
